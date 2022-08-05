d6a9ebfbc676ec2e71bef8a15c0ae8dcc776dbe4
dzindziura <romandzindzyura@softsprint.net>
2022-08-05 19:24:22 +0300

    basics home work #1

---
+++ src/basics/BasicsValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace basics;
+
+use InvalidArgumentException;
+
+class BasicsValidator implements BasicsValidatorInterface{
+    /**
+     * @param int $minute
+     * @throws InvalidArgumentException
+     */
+    public function isMinutesException(int $minute): void
+    {
+    if($minute<0 or $minute>59){
+        throw new InvalidArgumentException('Invalid');
+    }
+
+
+    }
+    /**
+    * @param int $year
+    * @return boolean
+    * @throws \InvalidArgumentException
+    */
+    public function isYearException(int $year): void
+    {
+        if($year<1900){
+            throw new InvalidArgumentException('Invalid');
+        }
+    }
+    /**
+     * @param string $input
+     * @throws \InvalidArgumentException
+     */
+    public function isValidStringException(string $input): void
+    {
+        if(mb_strlen($input)<6){
+            throw new InvalidArgumentException('Invalid');
+        }
+
+    }
+}
