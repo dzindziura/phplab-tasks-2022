61ea46aa9c417866046d052f2e524ea66ae4fc86
dzindziura <romandzindzyura@softsprint.net>
2022-08-11 19:32:59 +0300

    error correction

--- src/basics/Basics.php
+++ src/basics/Basics.php
@@ -2,13 +2,16 @@
 
 namespace basics;
 
-class Basics implements BasicsInterface{
+class Basics implements BasicsInterface
+{
 
     private $validator;
 
-    public function __construct($validator){
+    public function __construct($validator)
+    {
         $this->validator = $validator;
     }
+
     /**
      * @param int $minute
      * @return string
@@ -18,55 +21,51 @@ class Basics implements BasicsInterface{
     {
         $this->validator->isMinutesException($minute);
 
-            if($minute <= 15 and $minute >= 1){
-                return 'first';
-            }else if($minute >= 16 and $minute <= 30){
-                return 'second';
-            }else if($minute >= 31 and $minute <= 45){
-                return 'third';
-            }else if(($minute >= 46 and $minute <= 59) or $minute == 0){
-                return 'fourth';
-            }
+        if ($minute <= 15 && $minute >= 1) {
+            return 'first';
+        } elseif ($minute >= 16 && $minute <= 30) {
+            return 'second';
+        } elseif ($minute >= 31 && $minute <= 45) {
+            return 'third';
+        } elseif (($minute >= 46 && $minute <= 59) || $minute == 0) {
+            return 'fourth';
+        }
     }
+
     /**
-    * @param int $year
-    * @return boolean
-    * @throws \InvalidArgumentException
-    */
+     * @param int $year
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
     public function isLeapYear(int $year): bool
     {
+        $result = false;
         $this->validator->isYearException($year);
-        if($year%4==0){
-            if($year % 100 != 0 || ( $year % 100 == 0 && $year % 400 == 0)){
-                return true;
-            }
+        if (0 == $year % 4 && 0 != $year % 100 || 0 == $year % 400) {
+            $result = true;
         }
-
-        return false;
+        return $result;
     }
+
     /**
-    * @param string $input
-    * @return boolean
-    * @throws \InvalidArgumentException
-    */
+     * @param string $input
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
     public function isSumEqual(string $input): bool
     {
         $this->validator->isValidStringException($input);
-        $result = str_split(mb_substr($input, 0, 3));
-        $result2 = str_split(mb_substr($input, 3, 3));
+        $firstHalfString = str_split(mb_substr($input, 0, 3));
+        $secondHalfString = str_split(mb_substr($input, 3, 3));
 
-        $res1 = 0;
-        foreach ($result as $res){
-            $res1 += $res;
-        }
-        $res2 = 0;
-        foreach ($result2 as $res){
-            $res2 += $res;
-        }
-        if($res1 == $res2){
-            return true;
+        $sumFirstHalfString = 0;
+        $secondFirstHalfString = 0;
+
+        for ($i = 0; $i < count($firstHalfString); $i++) {
+            $sumFirstHalfString = $sumFirstHalfString + $firstHalfString[$i];
+            $secondFirstHalfString = $secondFirstHalfString + $secondHalfString[$i];
         }
 
-        return false;
+        return $sumFirstHalfString == $secondFirstHalfString;
     }
 }
--- src/basics/BasicsValidator.php
+++ src/basics/BasicsValidator.php
@@ -4,37 +4,40 @@ namespace basics;
 
 use InvalidArgumentException;
 
-class BasicsValidator implements BasicsValidatorInterface{
+class BasicsValidator implements BasicsValidatorInterface
+{
     /**
      * @param int $minute
      * @throws InvalidArgumentException
      */
     public function isMinutesException(int $minute): void
     {
-    if($minute<0 or $minute>59){
-        throw new InvalidArgumentException('Invalid');
-    }
+        if ($minute < 0 or $minute > 59) {
+            throw new InvalidArgumentException('Invalid');
+        }
 
 
     }
+
     /**
-    * @param int $year
-    * @return boolean
-    * @throws \InvalidArgumentException
-    */
+     * @param int $year
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
     public function isYearException(int $year): void
     {
-        if($year<1900){
+        if ($year < 1900) {
             throw new InvalidArgumentException('Invalid');
         }
     }
+
     /**
      * @param string $input
      * @throws \InvalidArgumentException
      */
     public function isValidStringException(string $input): void
     {
-        if(mb_strlen($input)<6){
+        if (strlen($input) < 6) {
             throw new InvalidArgumentException('Invalid');
         }
 
