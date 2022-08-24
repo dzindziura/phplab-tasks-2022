cb6fb5570a4ba81660036a0960c247116b1fcad3
dzindziura <romandzindzyura@softsprint.net>
2022-08-25 00:23:00 +0300

    arrays HW

--- src/arrays/Arrays.php
+++ src/arrays/Arrays.php
@@ -4,34 +4,25 @@ namespace arrays;
 
 class Arrays implements ArraysInterface
 {
-    /**
-     * @inheritDoc
-     */
-    public function repeatArrayValues(array $input) : array
+    public function repeatArrayValues(array $input): array
     {
         $result = [];
 
         foreach ($input as $inp) {
             $result += array_fill(count($result), $inp, $inp);
-            var_dump($result);
         }
 
         return $result;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function getUniqueValue(array $input) : int
+    public function getUniqueValue(array $input): int
     {
         $result = array_diff($input, array_diff_key($input, array_unique($input)));
+
         return $result ? min($result) : 0;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function groupByTag(array $input) : array
+    public function groupByTag(array $input): array
     {
         $result = [];
 
